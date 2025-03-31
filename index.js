a7156eefd5f83cc27092b79b55b5ca54754f3bdb
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-03-31 12:47:13 +0300

    using DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function(){
+    const text= document.getElementById('text');
+
+    display=text.textContent= 'This is really cool!';
+    //console.log(text);
+})
